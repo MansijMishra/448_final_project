379df2447ca1bf3abc5f57d182fd97cb76744569
JohanDelao <johandelao10@gmail.com>
2023-12-19 23:28:32 -0500

    feat: everything

--- app/web-app/app/Components/Nav.js
+++ app/web-app/app/Components/Nav.js
@@ -1,12 +1,15 @@
+"use client"
+
 import Image from 'next/image';
 import dog from '../../public/Dog.svg';
+import Link from 'next/link';
 
 function Nav(){
-    return <div className="flex max-h-16 w-full px-5 py-2 bg-white">
-        <div className="flex gap-3 items-center">
-            <Image src={dog} />
-            <h2 className='text-xl font-bold'>Breedpedia</h2>
-        </div>
+    return <div className="flex h-16 w-full px-5 py-2 bg-white overflow-hidden">
+        <Link href={"/"} className='flex gap-3 items-center'>
+                <Image src={dog} />
+                <h2 className='text-xl font-bold m-0'>Dogpedia</h2>
+        </Link>
     </div>
 }
 
--- app/web-app/app/page.js
+++ app/web-app/app/page.js
@@ -1,41 +1,92 @@
-import Image from "next/image"
+"use client";
 
-import Nav from "./Components/Nav"
-import Upload from "../public/upload.svg"
-import happyDog from "../public/happyDog.png"
+import Image from "next/image";
+import { useState } from "react";
 
-function Content(){
+import happyDog from "../public/happyDog.png";
+
+import { useRouter } from "next/navigation";
+
+function Content() {
+  const router = useRouter();
+
+  const [file, setFile] = useState();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+
+    try {
+      var formdata = new FormData();
+      formdata.append("file", file, "n02100583_216.jpg");
+
+      const url = URL.createObjectURL(file);
+
+      var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://74b2-54-83-177-126.ngrok-free.app/predict_breed",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          router.push(`/result?breed=${result.Breed}&imgSrc=${url}`);
+        })
+        .catch((error) => console.log("error", error));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div className="w-full h-full px-5 mt-5">
+    <div className="w-full h-full px-5 pt-5 ">
       <div className="flex flex-col md:grid md:grid-cols-3">
         <div className="flex flex-col col-span-2 gap-5 md:gap-10">
-          <h2 className="text-4xl md:text-5xl md:leading-snug font-bold">Snap, Click, Identify: Unraveling Breeds Through Photos!</h2>
+          <h2 className="text-4xl md:text-5xl md:leading-snug font-bold">
+            Snap, Click, Identify: Unraveling Breeds Through Photos!
+          </h2>
           <div className="flex flex-col gap-3">
-            <p>With over <strong>360</strong> recognized dog breeds worldwide, identifying a specific dog can be challenging unless one has taken the time to study each breed.</p>
-            <p>Breedpedia makes this <strong>easy</strong>, just upload an image below and get your information in just a <strong>few seconds!</strong>​</p>
+            <p>
+              With over <strong>360</strong> recognized dog breeds worldwide,
+              identifying a specific dog can be challenging unless one has taken
+              the time to study each breed.
+            </p>
+            <p>
+              Breedpedia makes this <strong>easy</strong>, just upload an image
+              below and get your information in just a{" "}
+              <strong>few seconds!</strong>​
+            </p>
           </div>
-          <div className="flex justify-center md:block">
-            <button className="max-h-12 flex max-w-72 w-56 justify-around px-3 rounded-md border-2 border-solid border-black py-2 items-center">
-              <Image src={Upload} />
-              <p className="text-black">Upload your Photo!</p>
-            </button>
+          <div className="flex md:block">
+            <form onSubmit={onSubmit} className="flex flex-col gap-2">
+              <input
+                type="file"
+                name="file"
+                onChange={(e) => setFile(e.target.files?.[0])}
+                className="w-fit"
+              />
+              <input type="submit" className="border-2 rounded-md border-black text-black w-20 cursor-pointer" value="Upload" />
+            </form>
           </div>
         </div>
         <div className="col-span-1">
-          <figure className='md:absolute md:bottom-0 md:right-0 md:w-4/12 mx-auto w-6/12'>
+          <figure className="md:absolute md:bottom-0 md:right-0 md:w-4/12 mx-auto w-6/12">
             <Image src={happyDog} />
           </figure>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default function Home() {
   return (
-   <div className='w-full min-h-screen bg-[#F5F5F5]'>
-    <Nav />
-    <Content />
-   </div>
-  )
+    <div className="w-full Main-screen">
+      <Content />
+    </div>
+  );
 }
